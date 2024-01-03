2ef4995948e04564ef79f505799069dfb5863b0f
Aditya <lian2696@gmail.com>
2024-01-03 23:10:45 +0530

    Add userscripts for ProductHunt and Reddit

---
+++ product-hunt/get_product_list.user.js
@@ -0,0 +1,89 @@
+// ==UserScript==
+// @name        Get Product List
+// @version     1.0
+// @namespace   https://github.com/adityarsuryavamshi
+// @description Copy products from product hunt time travel page into clipboard
+// @icon        https://ph-static.imgix.net/ph-favicon-coral.ico
+// @author      Aditya
+// @match       https://www.producthunt.com/time-travel/*
+// @grant       GM_registerMenuCommand
+// @grant       GM_setClipboard
+// @grant       GM_notification
+// ==/UserScript==
+
+
+function getProductList() {
+
+    const productList = Array.from(document.querySelectorAll('#__next > div.layoutContainer > div > div > div:nth-child(3) > div'));
+
+    const allProducts = []
+
+    for (const product of productList) {
+
+        // Logo
+        let productImgLogoURL;
+        try {
+            const productImgLogo = new URL(product.querySelector('li a img').src);
+            productImgLogoURL = productImgLogo.origin + productImgLogo.pathname;
+        } catch (err) { }
+
+        // Name and Description
+        const productName = product.querySelector('li > div > div:nth-of-type(1)').textContent;
+        const productDescription = product.querySelector('li > div > div:nth-of-type(2)').textContent;
+
+        // Comments
+        const commentCount = parseInt(product.querySelector('li > div > div:nth-of-type(3) > a').textContent.replaceAll(',', ''))
+
+        // Tags
+        const tags = Array.from(product.querySelectorAll('li > div > div:nth-of-type(3) > div a[href*=topic]'))
+            .map(e => e.textContent)
+            .map(e => e.replaceAll('#', ''));
+
+        // Votes
+        const voteCount = parseInt(product.querySelector('li > div:nth-of-type(2)').textContent.replaceAll(',', ''));
+
+        const productItem = {
+            productName: productName,
+            productDescription: productDescription,
+            productLogo: productImgLogoURL,
+            tags: tags,
+            commentCount: commentCount,
+            voteCount: voteCount,
+        }
+        allProducts.push(productItem)
+    }
+
+    return allProducts;
+}
+
+function onClick() {
+    const productList = getProductList();
+    GM_setClipboard(JSON.stringify(productList));
+    GM_notification({ text: "Copied Product List onto Clipboard!" })
+}
+
+
+let lastScrollHeight;
+let retryCount = 0;
+
+
+function scrollToEnd() {
+    const currentScrollHeight = document.body.scrollHeight;
+    if (currentScrollHeight === lastScrollHeight) {
+        retryCount += 1
+    } else {
+        retryCount = 0
+    }
+
+    if (retryCount < 10) {
+        scroll(0, currentScrollHeight);
+        lastScrollHeight = currentScrollHeight;
+        setTimeout(scrollToEnd, 300);
+    } else {
+        onClick();
+    }
+
+}
+
+GM_registerMenuCommand('Get Products', onClick)
+scrollToEnd()
---
+++ reddit/redirect_to_old.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Redirect to Old Reddit
+// @version     1.0
+// @namespace   https://github.com/adityarsuryavamshi
+// @description Force reddit to take old reddit interface
+// @icon        https://www.redditstatic.com/desktop2x/img/favicon/android-icon-512x512.png
+// @author      Aditya
+// @match       https://www.reddit.com/r/*
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+
+location.href = location.href.replace('www', 'old');
